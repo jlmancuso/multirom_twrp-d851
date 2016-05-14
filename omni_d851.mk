9e3eeb8ad0de53edff62491af89ee7d71e0c78f6
bryan2894 <bryan2894@aospal.com>
2016-05-14 12:46:25 -0400

    d851: update kernel

--- omni_d851.mk
+++ omni_d851.mk
@@ -50,8 +50,8 @@ PRODUCT_MANUFACTURER := LG
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_tmo_us" \
-    BUILD_FINGERPRINT="lge/g3_tmo_us/g3:5.0.1/LRX21Y/15227151285fc:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_tmo_us-user 5.0.1 LRX21Y 15227151285fc release-keys"
+    BUILD_FINGERPRINT="lge/g3_tmo_us/g3:6.0/MRA58K/1609517159bca:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_tmo_us-user 6.0 MRA58K 1609517159bca release-keys"
 
 # Kernel inline build
 #TARGET_KERNEL_CONFIG := d851_defconfig
